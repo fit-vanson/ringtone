19c7e91b56a570302db6ba5d6893b6b6d84261d7
Administrator <fit.vanson@gmail.com>
2022-01-13 10:35:21 +0700

    update

--- resources/views/content/policy.blade.php
+++ resources/views/content/policy.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel="shortcut icon" href="{{asset('storage/sites/'. preg_replace('/[^A-Za-z0-9\-._]/',"/",$site->header_image))}}">
+    <link rel="shortcut icon" href="{{asset('storage/sites/'.$site->header_image)}}">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
@@ -15,7 +15,7 @@
         .header {
             padding: 60px;
             text-align: center;
-            background-image: url({{asset('storage/sites/'. preg_replace('/[^A-Za-z0-9\-._]/',"/",$site->header_image))}});
+            background-image: url({{asset('storage/sites/'.$site->header_image)}});
             color: white;
             font-size: 30px;
             margin-bottom: 30px;
