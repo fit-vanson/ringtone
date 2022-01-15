c9857e7e33bdf8b0f54908f4a0c84491622c6c58
Administrator <fit.vanson@gmail.com>
2022-01-15 14:36:09 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\FeatureImage;
 
+
+use App\Models\Ringtone;
 use App\Models\SiteManage;
 use App\Models\User;
 use Carbon\Carbon;
+use Cloudflare\API\Adapter\Guzzle;
+use Cloudflare\API\Auth\APIKey;
+use Cloudflare\API\Endpoints\DNS;
+use Cloudflare\API\Endpoints\DNSAnalytics;
 use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Session\Store;
@@ -16,7 +22,14 @@ use ImageOrientationFix\ImageOrientationFixer;
 use Illuminate\Support\Facades\Validator;
 use Intervention\Image\Facades\Image;
 use Monolog\Logger;
+//use Spatie\Analytics\Analytics;
+use Spatie\Analytics\Period;
 use Spatie\Permission\Models\Role;
+//use Wappr\Cloudflare\AnalyticsClient;
+//use Wappr\Cloudflare\Resources\Account;
+//use Wappr\Cloudflare\DataSets\HttpRequests\HttpRequests1dGroups;
+//use Wappr\Cloudflare\SelectionSets\HttpRequests\HttpRequestsSum;
+use Spatie\Analytics\AnalyticsFacade as Analytics; //Change here
 
 class HomeController extends Controller
 {
@@ -51,6 +64,34 @@ class HomeController extends Controller
 
     public function home()
     {
+        $analytics = Analytics::fetchMostVisitedPages(Period::days(1));
+        dd($analytics);
+
+
+        $key     = new APIKey('ngocphandang@yahoo.com.vn', 'f4fb1dd91d4a7abce9460fe85f0cec82a6a69');
+        $adapter = new Guzzle($key);
+        $user    = new \Cloudflare\API\Endpoints\User($adapter);
+//        dd($user->getUserID());
+        $DNSAnalytics = new DNSAnalytics($adapter);
+        $DNS = new DNS($adapter);
+        $zoneID = '66ba85f3ec062939ed1098ffd82838e1';
+        $dimensions = ["responseCode"]; //'queryName',
+        $metrics = [ 'queryCount' ];
+        $sort = [ ""];
+
+//        adcc81574262d4b0cb57de58f6ed9967
+
+
+
+//        $dataSet = new HttpRequestsSum();
+
+//        $until = '2022-01-14T23:59:00Z';
+        $until = Carbon::now()->toISOString(); //toISOString  ,toAtomString
+//        dd($until);
+        $since = Carbon::now()->subHour(6)->toISOString();  //subHour
+
+//        dd($DNS->getRecordDetails($zoneID,'2960611a52aa4a9619c7452bef1f389d'));
+        dd($DNSAnalytics->getReportTable($zoneID,$dimensions,$metrics,$sort,'',$since,$until,100));
         return view('content.home');
     }
 
